4361cacc669ef5309477da3566506a72b687ca29
William Schwarz <wschwarz@ucla.edu>
2011-05-28 13:54:23 -0700

    added landing page

--- bookmarktagger.php
+++ bookmarktagger.php
@@ -1,29 +1,35 @@
 <?php
 //error_reporting(E_ALL);
+function lb()
+{
+	if(defined('STDIN') ) 
+		return "\r\n"; 
+	else 
+		return "<br />"; 
+}
+
 
 function getTags($url)
 {
-	echo "Getting tags\r\n";
+	echo "Getting tags" + lb();
 	$curlUrl = "https://wschwarz1986:tagsort1scool@api.del.icio.us/v1/posts/suggest?url=" . $url;
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $curlUrl);
-	//curl_setopt($curl, CURLOPT_HEADER, true);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($curl, CURLOPT_VERBOSE, true);
-	//curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	//curl_setopt($curl, CURLOPT_VERBOSE, true); //enable for extra msgs.
 	
 	$result = curl_exec($curl);
 		
 	print_r($result);
-	print_r(curl_error($curl));
+	error_log(curl_error($curl));
 	$returnArr = array();
 	$xml = new SimpleXMLElement($result);
 	foreach($xml->popular as $item ) {
-		print_r("\r\ntag:" . $item);
+		print_r(lb() + "tag:" . $item);
 		array_push($returnArr, $item);
 	}
-	echo "\r\nEnd get tags";	
+	echo lb() + "End get tags";	
 	curl_close($curl);
 	return $returnArr;
 }
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Tag Sort</title>
+</head>
+<body>
+<h1>Coming soon...</h1>
+<br />
+<p>Tag Sort is going to be a service offered through a Chrome extension that
+will allow automatic sorting of bookmarks. If you would like to contribute or
+have any questions or comments please contact me at 
+<a href="mailto:wschwarz1986@gmail.com?Subject=TagSort"></a>
+</p>
+</body>
+</html>
