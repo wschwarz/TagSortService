7ad4a3f1e5ed49d63379421ccba3c2170f9536c4
William Schwarz <wschwarz@ucla.edu>
2011-05-28 10:52:09 -0700

    Adding service files

---
+++ BookmarkReceiverService.php
@@ -0,0 +1,69 @@
+<?php
+/* This file is a webservice that receives bookmark data from a 
+chrome extension one entry at a time and inserts it into a 
+mongo db database */
+
+include 'bookmarktagger.php';
+//include 'dbAccess.php';
+//$db = new db();
+
+function csvEcho($tags)
+{
+	$responseText = "";
+	foreach($tags as $tag)
+	{
+		$responseText .= $tag . ',';
+	}		
+	echo substr($responseText, 0, strlen($responseText) - 1);
+}
+
+function ReceiveFromPost() {
+	$url_array = parse_url($_POST["url"]);
+	$entry = array("title" => $_POST["title"],
+			"link" => $_POST["link"],
+			"add_date" => $_POST["add_date"],
+			"last_modified" => $_POST["last_modified"],
+			"category" => $_POST["category"],
+			"host" => $url_array['host'],
+			"path" => $url_array['path'],
+			"user" => 1,
+			"read" => "No");
+	return $entry;
+}
+
+function ReceiveAsXML() {
+	$xml = $GLOBALS['HTTP_RAW_POST_DATA'];
+	$xml = str_replace("&","&amp;", $xml );
+	$xmlParsed = new SimpleXMLElement($xml);
+	$url_array = parse_url((string)$xmlParsed->link);
+	$entry = array("title" => (string)$xmlParsed->title,
+			"link" => (string)$xmlParsed->link,
+			"add_date" => (string)$xmlParsed->add_date,
+			"last_modified" => (string)$xmlParsed->last_modified,
+			"category" => (string)$xmlParsed->category,
+			"host" => $url_array['host'],
+			"path" => $url_array['path'],
+			"user" => 1,
+			"read" => "No");
+	return $entry;
+}
+// Receive a bookmark
+if (strstr($_SERVER["CONTENT_TYPE"], "application/x-www-form-urlencoded") != FALSE) {
+	error_log("Starting Post string Processing <br />");
+	//$entry = ReceiveFromPost();
+	//$db->insert($entry);
+	$tags = getTags($entry["link"]);
+	csvEcho($tags);
+	error_log($entry["title"] . " added!");
+}
+else if (strstr($_SERVER["CONTENT_TYPE"], "text/xml") != FALSE) {
+	error_log("Starting XML Processing <br />");
+	$entry = ReceiveAsXML();
+	//$db->insert($entry);
+	$tags = getTags($entry["link"]);
+	csvEcho($tags);
+	error_log($entry["title"] . " added!");
+}
+else error_log("Invalid Post data <br />");
+//$db->close();
+?>
---
+++ bookmarktagger.php
@@ -0,0 +1,32 @@
+<?php
+//error_reporting(E_ALL);
+
+function getTags($url)
+{
+	echo "Getting tags\r\n";
+	$curlUrl = "https://wschwarz1986:t@gsortiscool@api.del.icio.us/v1/posts/suggest?url=" . $url;
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $curlUrl);
+	//curl_setopt($curl, CURLOPT_HEADER, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+	//curl_setopt($curl, CURLOPT_VERBOSE, true);
+	//curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	
+	$result = curl_exec($curl);
+		
+	print_r($result);
+	error_log(curl_error($curl));
+	$returnArr = array();
+	$xml = new SimpleXMLElement($result);
+	foreach($xml->popular as $item ) {
+		print_r("\r\ntag:" . $item);
+		array_push($returnArr, $item);
+	}
+	echo "\r\nEnd get tags";	
+	curl_close($curl);
+	return $returnArr;
+}
+
+getTags("http://www.google.com");
+?>
